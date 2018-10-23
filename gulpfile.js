15f362d080170e1819ffd69d854280f80c5854ce
lysenkolipa <olimpiada_l@ukr.net>
2018-10-23 09:18:33 +0300

    added files

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require('gulp'),
+      browserSync = require('browser-sync').create(),
+      sass = require('gulp-sass'),
+      rimraf = require('rimraf'),
+      rename = require('gulp-rename'),
+      uglify = require('gulp-uglify'),
+      concat = require('gulp-concat'),
+      sourcemaps = require('gulp-sourcemaps');
+
+/*------------  Server  -------------*/
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+/*------------ HTML compile -------------*/
+gulp.task('templates:compile', function buildHTML() {
+    return gulp.src('source/index.html')
+        .pipe(gulp.dest('build'))
+});
+
+/*------------ Styles compile -------------*/
+gulp.task('styles:compile', function () {
+    return gulp.src('source/styles/main.scss')
+        .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(rename('main.min.css'))
+        .pipe(gulp.dest('build/css'));
+});
+
+/*------------ JS -------------*/
+gulp.task('js',function() {
+    return gulp.src('source/js/*.js')
+        .pipe(gulp.dest('build/js'));
+});
+
+/*------------ Delete -------------*/
+gulp.task('clean', function del(cb){
+    return rimraf('build', cb);
+});
+
+/*------------ Copy images -------------*/
+gulp.task('copy:images', function(){
+    return gulp.src('./source/images/**/*.*')
+        .pipe(gulp.dest('build/images'));
+});
+
+/*------------ Copy -------------*/
+gulp.task('copy', gulp.parallel('copy:images'));
+
+/*------------ Watchers -------------*/
+gulp.task('watch', function() {
+    gulp.watch('source/**/*.html', gulp.series('templates:compile'));
+    gulp.watch('source/styles/**/*.scss', gulp.series('styles:compile'));
+    gulp.watch('source/js/**/*.js', gulp.series('js'));
+});
+
+/*------------ Gulp default -------------*/
+gulp.task('default', gulp.series(
+    'clean',
+    gulp.parallel('templates:compile', 'styles:compile', 'js', 'copy'),
+    gulp.parallel('watch', 'server')
+    )
+);
+
+
